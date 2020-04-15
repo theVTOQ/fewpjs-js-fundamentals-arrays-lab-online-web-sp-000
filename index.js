2609f59dbb1bd5102ef347aa702cfecbe3f6e466
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2020-04-15 22:34:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 // Write your solution here!
-const append = ["Milo", "Otis", "Garfield"]
-const prepend = append.slice()
-const removeLast = append.slice()
-const removeFirst = append.slice()
+const append = ["Milo", "Otis", "Garfield"];
+const prepend = append.slice();
+const removeLast = append.slice();
+const removeFirst = append.slice();
+
+append.push("Odie");
+prepend.unshift("Odie");
+removeLast.splice(-1);
+removeFirst.shift()

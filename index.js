65e3b659bea2dd53532acbfaafe2e546af25a0f4
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2020-04-15 22:35:47 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your solution here!
+const append = ["Milo", "Otis", "Garfield"];
+const prepend = append.slice();
+const removeLast = append.slice();
+const removeFirst = append.slice();
+
+append.push("Odie");
+prepend.unshift("Odie");
+removeLast.pop();
+removeFirst.shift()
